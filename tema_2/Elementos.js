1e6765dfcdbd0529c850028d8f1bcfb895c8de46
DBordallo <117821904+DBordallo@users.noreply.github.com>
2023-01-21 13:00:21 +0100

    tema_2

---
+++ tema_2/Elementos.js
@@ -0,0 +1,19 @@
+const nombre = "Diego";
+console.log (nombre);
+
+const edad = 37;
+console.log (edad);
+
+const desarrolador = false;
+console.log (desarrolador);
+
+const cumpleaños = new Date (1986, 0, 03);
+console.log (cumpleaños);
+
+const libro = {
+    título : "El principito",
+    autor : "Antoine de Saint Exupery", 
+    fecha : "6 de abril de 1943", 
+    enlace :"https://www.casadellibro.com/libro-el-principito/9788478887194/795940"
+}
+console.log (libro);
