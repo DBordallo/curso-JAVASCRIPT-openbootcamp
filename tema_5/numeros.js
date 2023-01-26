8d874c0ae6f5d05a0a2af22c731925b9149f02ab
DBordallo <117821904+DBordallo@users.noreply.github.com>
2023-01-26 13:18:40 +0100

    tema_5

---
+++ tema_5/numeros.js
@@ -0,0 +1,27 @@
+let alturacm = 178;
+
+
+let alturam = 1.78;
+
+
+let pesokl = 82.5;
+
+
+let redondeoarriba = alturam.toFixed(0)
+console.log(redondeoarriba)
+
+
+
+let redondeoabajo = Math.floor(pesokl)
+console.log(redondeoabajo)
+
+
+
+let maximoValor = (Number.MAX_VALUE + 1)
+console.log(maximoValor +1)
+
+let maximo = Number.MAX_VALUE
+console.log(maximo)
+
+Number.MAX_VALUE = (Number.MAX_VALUE + 1)
+
